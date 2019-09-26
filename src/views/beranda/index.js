1080ab0cbe259e8a6f8291d3610b743e5b31ad60
Yusuf Ayuba <yusufayuba@live.com>
2019-09-26 10:20:05 +0800

    #9, add views page

---
+++ src/views/beranda/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Beranda = () => {
+    return (
+        <>
+            Beranda
+        </>
+    );
+}
+
+export default Beranda;
---
+++ src/views/portofolio/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portofolio = () => {
+    return (
+        <>
+            Portolio
+        </>
+    );
+}
+
+export default Portofolio;
