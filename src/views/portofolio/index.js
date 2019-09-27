d1425b3255d928218f82a5df3bdaf25eaeb982f0
Yusuf Ayuba <yusufayuba@live.com>
2019-09-27 23:42:09 +0800

    #62, change layout

--- src/views/portofolio/index.js
+++ src/views/portofolio/index.js
@@ -1,47 +1,35 @@
 import React, {useEffect} from 'react';
-
+import {Link} from 'react-router-dom';
 import Section from 'react-bulma-components/lib/components/section';
 //import Container from 'react-bulma-components/lib/components/container';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Columns from 'react-bulma-components/lib/components/columns';
-import Image from 'react-bulma-components/lib/components/image';
+import LazyImage from '../../components/images';
+import data from './data';
 
 const Portofolio = () => {
-
+    const datas = data.items;
     useEffect(() => {
         document.title = 'Portofolio';
-    }, [])
+    }, []);
 
     return (
         <Section>
-            <Heading size={3} renderAs="div" style={{textAlign: 'center'}}>Project yang telah dikerjakan</Heading>
+            <Heading size={3} renderAs="div" style={{textAlign: 'center'}}>Project yang pernah/sedang dikerjakan</Heading>
             <Heading size={5} subtitle renderAs="div" style={{textAlign: 'center'}}>Klik untuk detailnya</Heading>
             <Columns>
-                <Columns.Column>
-                    <Image 
-                        src="https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lp-lan" 
-                        size="1by1"
-                    />
-                    <Heading size={6} subtitle renderAs="div" style={{textAlign: 'center'}}>Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)</Heading>
-                </Columns.Column>
-                <Columns.Column>
-                    <Image 
-                        src="https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lp-lan" 
-                        size={128}
-                    />
-                </Columns.Column>
-                <Columns.Column>
-                    <Image 
-                        src="https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lp-lan" 
-                        size={128}
-                    />
-                </Columns.Column>
-                <Columns.Column>
-                    <Image 
-                        src="https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lp-lan" 
-                        size={128}
-                    />
-                </Columns.Column>
+                {
+                    datas.map((item, key) => {
+                        return (
+                            <Columns.Column size={3} key={key}>
+                                <Link to="/">
+                                    <LazyImage src={item.thumbs} />
+                                    <Heading size={6} subtitle renderAs="div" style={{textAlign: 'center'}}>{item.title}</Heading>
+                                </Link>
+                            </Columns.Column>
+                        );
+                    })
+                }
             </Columns>
         </Section>
     );
