2b5781a7c08967aea942303b0f428abc189d7160
Yusuf Ayuba <yusufayuba@live.com>
2019-09-27 22:43:15 +0800

    #60, add data portofolio

---
+++ src/views/portofolio/data.js
@@ -0,0 +1,68 @@
+export default {
+    items: [
+        {
+            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
+            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
+            tahun: '2015',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
+            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
+            tahun: '2010',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
+            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
+            tahun: '2015',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
+            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
+            tahun: '2010',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
+            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
+            tahun: '2015',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
+            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
+            tahun: '2010',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
+            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
+            tahun: '2015',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+        {
+            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
+            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
+            tahun: '2010',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+
+        },
+    ]
+}
