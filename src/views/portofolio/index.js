dce42f15f08e9aca54cd8181db20a675de09ee16
Yusuf Ayuba <yusufayuba@live.com>
2019-09-28 15:43:38 +0800

    #69, add skeleton loader

--- src/views/portofolio/index.js
+++ src/views/portofolio/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import {Skeleton} from '@material-ui/lab';
+
 import Section from 'react-bulma-components/lib/components/section';
-//import Container from 'react-bulma-components/lib/components/container';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Columns from 'react-bulma-components/lib/components/columns';
 import LazyImage from '../../components/images';
@@ -9,10 +10,13 @@ import { project } from '../../components/firebase';
 
 const Portofolio = () => {
     const [datas, setDatas] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
     const getProject = () => {
+        setLoading(true);
         project.on('value', snap => {
             setDatas(snap.val());
+            setLoading(false);
         })
     }
 
@@ -21,24 +25,58 @@ const Portofolio = () => {
         getProject();
     }, []);
 
+    //console.log(isLoading);
     return (
         <Section>
-            <Heading size={3} renderAs="div" style={{textAlign: 'center'}}>Project yang pernah/sedang dikerjakan</Heading>
-            <Heading size={5} subtitle renderAs="div" style={{textAlign: 'center'}}>Klik untuk detailnya</Heading>
-            <Columns>
-                {
-                    datas.map((item, key) => {
-                        return (
-                            <Columns.Column size={3} key={key}>
-                                <Link to="/">
-                                    <LazyImage src={item.thumb} />
-                                    <Heading size={6} subtitle renderAs="div" style={{textAlign: 'center'}}>{item.title}</Heading>
-                                </Link>
+            {
+                isLoading ? 
+                    <>
+                        <Heading size={3} renderAs="div"><Skeleton variant="rect" height={20} width={600} /></Heading>
+                        <Heading subtitle size={3} renderAs="div"><Skeleton variant="rect" height={17} width={200} /></Heading>
+                        <Columns>
+                            <Columns.Column size={3}>
+                                <Skeleton variant="rect" height={150} width={250} />
+                                <Skeleton variant="rect" height={15} width={250} style={{marginTop: 10}} />
+                                <Skeleton variant="rect" height={15} width={100} style={{marginTop: 10}} />
                             </Columns.Column>
-                        );
-                    })
-                }
-            </Columns>
+                            <Columns.Column size={3}>
+                                <Skeleton variant="rect" height={150} width={250} />
+                                <Skeleton variant="rect" height={15} width={250} style={{marginTop: 10}} />
+                                <Skeleton variant="rect" height={15} width={100} style={{marginTop: 10}} />
+                            </Columns.Column>
+                            <Columns.Column size={3}>
+                                <Skeleton variant="rect" height={150} width={250} />
+                                <Skeleton variant="rect" height={15} width={250} style={{marginTop: 10}} />
+                                <Skeleton variant="rect" height={15} width={100} style={{marginTop: 10}} />
+                            </Columns.Column>
+                            <Columns.Column size={3}>
+                                <Skeleton variant="rect" height={150} width={250} />
+                                <Skeleton variant="rect" height={15} width={250} style={{marginTop: 10}} />
+                                <Skeleton variant="rect" height={15} width={100} style={{marginTop: 10}} />
+                            </Columns.Column>
+                        </Columns>
+                    </>
+                :
+                    <>
+                        <Heading size={3} renderAs="div">Project yang pernah/sedang dikerjakan</Heading>
+                        <Heading size={5} subtitle renderAs="div">Klik untuk detailnya</Heading>
+                        <Columns>
+                            {
+                                datas.map((item, key) => {
+                                    return (
+                                        <Columns.Column size={3} key={key}>
+                                            <Link to="/">
+                                                <LazyImage src={item.thumb} />
+                                                <Heading size={6} subtitle renderAs="div" style={{textAlign: 'center'}}>{item.title}</Heading>
+                                            </Link>
+                                        </Columns.Column>
+                                    );
+                                })
+                            }
+                        </Columns>
+                    </>
+            }
+            
         </Section>
     );
 }
