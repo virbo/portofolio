17ebcea25df6e18cf2b4bcd66181677b7613ccb5
Yusuf Ayuba <yusufayuba@live.com>
2019-09-28 14:46:48 +0800

    #67, update data (for backup)

--- src/views/portofolio/data.js
+++ src/views/portofolio/data.js
@@ -1,6 +1,7 @@
 export default {
     items: [
         {
+            id: '1',
             title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
             description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
             tahun: '2015',
@@ -9,6 +10,7 @@ export default {
 
         },
         {
+            id: '2',
             title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
             description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
             tahun: '2010',
@@ -17,51 +19,39 @@ export default {
 
         },
         {
-            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
-            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
-            tahun: '2015',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+            id: '3',
+            title: 'Aplikasi Pengisian Biodata Alumni',
+            description: 'Aplikasi ini dikhususkan untuk memfasilitasi alumni dalam perubahan biodata di Pangkalan Data Pendidikan Tinggi (PDDIKTI)',
+            tahun: '2014',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/biodata_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/biodata.png',
 
         },
         {
-            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
-            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
-            tahun: '2010',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+            id: '4',
+            title: 'Aplikasi Repository Politeknik Kelautan dan Perikanan Sorong',
+            description: 'Aplikasi yang berfungsi sebagai wadah penyimpanan dokumen-dokumen digital yang dapat dibagi ke unit-unit kerja terdaftar',
+            tahun: '2016',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/repository_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/repository.png',
 
         },
         {
-            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
-            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
-            tahun: '2015',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+            id: '5',
+            title: 'Aplikasi Direktori Alumni Politeknik Kelautan dan Perikanan Sorong',
+            description: 'Aplikasi yang berfungsi pelacak/tracert data alumni. Aplikasi terdaftar fitur menampilkan lokasi Alumni berdasarkan alamat tinggal',
+            tahun: '2016',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/direktorialumni_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/direktorialumni.png',
 
         },
         {
-            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
-            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
-            tahun: '2010',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
-
-        },
-        {
-            title: 'Membangun jaringan LAN dan WAN PT Luwuk Cemerlang (Luwuk Post)',
-            description: 'Membangun jaringan LAN/WAN PT. Luwuk Cemerlang di kantor baru jalan Imam Bonjol KM. 3',
-            tahun: '2015',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
-
-        },
-        {
-            title: 'Membangun jaringan LAN/WAN dan Hotspot Akademi Keperawatan Luwuk',
-            description: 'Membangun jaringan LAN/WAN Akademi Keperawatan Luwuk untuk mendukung kegiatan Akademik',
-            tahun: '2010',
-            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp_thumb.jpg',
-            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/lanlp.jpg',
+            id: '6',
+            title: 'Aplikasi Sistem Informasi Akademik Politeknik Kelautan dan Perikanan Sorong',
+            description: 'Sistem Informasi Akademik yang terintegrasi dengan Aplikasi PDDIKTI Feeder',
+            tahun: '2017',
+            thumbs: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/siakadsorong_thumb.jpg',
+            img: 'https://s3-id-jkt-1.kilatstorage.id/cdn-yusufayuba/assets/img/project/siakadsorong.png',
 
         },
     ]
