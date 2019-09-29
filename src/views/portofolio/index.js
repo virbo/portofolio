4f48d0ccd4bc3876f30571d23664a6636f220cbb
Yusuf Ayuba <yusufayuba@live.com>
2019-09-29 12:39:22 +0800

    #76, code split to list and detail

--- src/views/portofolio/index.js
+++ src/views/portofolio/index.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
-import {Skeleton} from '@material-ui/lab';
-
 import Content from 'react-bulma-components/lib/components/content';
-import Heading from 'react-bulma-components/lib/components/heading';
-import Columns from 'react-bulma-components/lib/components/columns';
-import LazyImage from '../../components/images';
 import { project } from '../../components/firebase';
+import List from './list';
 
 const Portofolio = () => {
     const [datas, setDatas] = useState([]);
@@ -25,58 +20,9 @@ const Portofolio = () => {
         getProject();
     }, []);
 
-    //console.log(isLoading);
     return (
         <Content>
-            {
-                isLoading ? 
-                    <Content>
-                        <Heading size={3} renderAs="div"><Skeleton variant="rect" height={20} width="75%" /></Heading>
-                        <Heading subtitle size={3} renderAs="div"><Skeleton variant="rect" height={17} width="50%" /></Heading>
-                        <Columns>
-                            <Columns.Column size={3}>
-                                <Skeleton variant="rect" height={150} />
-                                <Skeleton variant="rect" height={15} style={{marginTop: 10}} />
-                                <Skeleton variant="rect" height={15} width="50%" style={{marginTop: 10}} />
-                            </Columns.Column>
-                            <Columns.Column size={3}>
-                                <Skeleton variant="rect" height={150} />
-                                <Skeleton variant="rect" height={15} style={{marginTop: 10}} />
-                                <Skeleton variant="rect" height={15} width="50%" style={{marginTop: 10}} />
-                            </Columns.Column>
-                            <Columns.Column size={3}>
-                                <Skeleton variant="rect" height={150} />
-                                <Skeleton variant="rect" height={15} style={{marginTop: 10}} />
-                                <Skeleton variant="rect" height={15} width="50%" style={{marginTop: 10}} />
-                            </Columns.Column>
-                            <Columns.Column size={3}>
-                                <Skeleton variant="rect" height={150} />
-                                <Skeleton variant="rect" height={15} style={{marginTop: 10}} />
-                                <Skeleton variant="rect" height={15} width="50%" style={{marginTop: 10}} />
-                            </Columns.Column>
-                        </Columns>
-                    </Content>
-                :
-                    <Content>
-                        <Heading size={3} renderAs="div">Project yang pernah/sedang dikerjakan</Heading>
-                        <Heading size={5} subtitle renderAs="div">Klik untuk detailnya</Heading>
-                        <Columns>
-                            {
-                                datas.map((item, key) => {
-                                    return (
-                                        <Columns.Column size={3} key={key}>
-                                            <Link to="/">
-                                                <LazyImage src={item.thumb} />
-                                                <Heading size={6} subtitle renderAs="div" style={{textAlign: 'center'}}>{item.title}</Heading>
-                                            </Link>
-                                        </Columns.Column>
-                                    );
-                                })
-                            }
-                        </Columns>
-                    </Content>
-            }
-            
+            <List data={datas} loading={isLoading} />
         </Content>
     );
 }
