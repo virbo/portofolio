e58f17904ce09d5a5de418098467a4831feb3589
Yusuf Ayuba <yusufayuba@live.com>
2019-10-01 09:57:27 +0800

    #87, add button detail

--- src/views/beranda/pengalaman.js
+++ src/views/beranda/pengalaman.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Columns from 'react-bulma-components/lib/components/columns';
 import Card from 'react-bulma-components/lib/components/card';
 import Media from 'react-bulma-components/lib/components/media';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Content from 'react-bulma-components/lib/components/content';
+import Button from 'react-bulma-components/lib/components/button';
 import { Skeleton } from '@material-ui/lab';
 
 const Pengalaman = (props) => {
     const data = props.data;
     const loading = props.loading;
+
+    const [idItem, setId] = useState(0);
+    const [isShow, setShow] = useState(false);
+
+    const showDetail = (id) => {
+        const _get = data.find((test) =>test.id === id);
+        setId(id);
+        setShow(_get ? !isShow : null);
+    }
 
     return loading ? <LoadingSkeleton /> : (
         <Columns>
@@ -25,7 +35,8 @@ const Pengalaman = (props) => {
                                             <Heading subtitle size={6}>{item.periode}</Heading>
                                         </Media.Item>
                                     </Media>
-                                    <Content>{item.description}</Content>
+                                    <Content>{isShow && item.id === idItem ? item.description : null}</Content>
+                                    <Button color="info" onClick={() => showDetail(item.id)}>{isShow && item.id === idItem ? 'Tutup' : 'Detail'}</Button>
                                 </Card.Content>
                             </Card>
                         );
