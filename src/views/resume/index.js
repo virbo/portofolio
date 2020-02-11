8c0958267feb8cdd463c8f3ed6ef49dc58c811e5
Yusuf Ayuba <yusufayuba@live.com>
2020-02-11 09:25:03 +0800

    #95, update belum fixed

--- src/views/resume/index.js
+++ src/views/resume/index.js
@@ -1,14 +1,43 @@
 import React, {useEffect} from 'react';
 
-const Resume = () => {
+import Heading from 'react-bulma-components/lib/components/heading';
+import Container from 'react-bulma-components/lib/components/container';
+import Content from 'react-bulma-components/lib/components/content';
+import Section from 'react-bulma-components/lib/components/section';
+import Button from 'react-bulma-components/lib/components/button';
+
+import {PDFExport} from '@progress/kendo-react-pdf';
+
+const Resume = (props) => {
+    //const resumes;
 
     useEffect(() => {
         document.title = 'Resume';
-    }, [])
+    }, []);
+
+    const simpanPdf = () => {
+        props.save();
+    }
+
     return (
-        <>
-            Resume
-        </>
+        <Content>
+            <Heading size={3} renderAs="div">
+                <Button color="info" onClick={() => simpanPdf()}>Download</Button>
+            </Heading>
+            
+            <PDFExport 
+                paperSize={'Letter'}
+                fileName="YusufAyubaResume.pdf"
+                title="Yusuf Ayuba Resume"
+                subject="Subject Resume"
+                keywords="yusufayuba, resume, portofolio"
+                ref={(r) => props = r}
+            >
+                <Content>
+                    Isi Resume
+                </Content>
+            </PDFExport>
+        </Content>
     );
 }
 
